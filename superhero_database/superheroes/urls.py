9c5910fc0d0adafaae00d43990ae33c3d8d2f986
unknown <ssgsj.carranza@gmail.com>
2021-04-07 21:59:28 -0500

    adding a css sheet and trying to solve a template does not exsist error

--- superhero_database/superheroes/urls.py
+++ superhero_database/superheroes/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 
 app_name = 'superheroes'
 urlpatterns = [
-    path('', views.index, name='index')
-    path(' <int:superheroes_id>/', views.detail, name='detail')
+    path('', views.index, name='index'),
+    path('<int:superheroes_id>/', views.detail, name='detail'),
+    path('new/', views.create, name='create_new_superhero'),
+    path('delete/', views.delete, name='id')
 ]
--- superhero_database/superheroes/views.py
+++ superhero_database/superheroes/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Superheroes
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 
 # Create your views here.
@@ -11,4 +13,34 @@ def index(request):
     context = {
         'all_superheroes': all_superheroes
     }
-    return render(request, "superheroes/index.html")
+    return render(request, "superheroes/index.html", context)
+
+
+def detail(request, superheroes_id):
+    all_superheroes = Superheroes.objects.all()
+    context = {
+        'all_superheroes': Superheroes.objects.filter()
+    }
+    return render(request, str(superheroes_id), "superheroes/index.html", context)
+
+
+def create(request):
+    if request.method == 'POST':
+        superhero_name = request.POST.get('superhero name')
+        alter_ego = request.POST.get('alter ego')
+        primary_ability = request.POST.get('primary ability')
+        secondary_ability = request.POST.get('secondary ability')
+        catchphrase = request.POST.get('catchphrase')
+        new_superhero = Superheroes(superhero_name=superhero_name, alter_ego=alter_ego, primary_ability=primary_ability,
+                                    secondary_ability=secondary_ability, catchphrase=catchphrase)
+        new_superhero.save()
+        return HttpResponseRedirect(reverse('superheroes:index'))
+    else:
+        return render(request, 'superheroes/create.html')
+
+
+def delete(conn, id):
+    sql = 'DELETE FROM tasks WHERE id=?'
+    cur = conn.cursor()
+    cur.execute(sql, (id,))
+    conn.commit()
